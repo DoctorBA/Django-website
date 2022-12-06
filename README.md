2432776b434c8a134088805c20f88186a2efaec8
DBA <7777_7788@mail.ru>
2022-12-06 16:52:16 +0300

    v1.2. Lesson 28.11.2022

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Django Shop:
 a) Fill in the data.
 b) Create a 'Cart' app. Add a Cart model.
 c) Create a migration and apply it.
+
+    v1.2. Lesson 28.11.2022 update:
+a) Add app accounts
+b) Added file html
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('catalog.urls')),
     path('cart/', include('cart.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
